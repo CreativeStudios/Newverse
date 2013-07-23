b962cbf2a5e3fffe1a46f6cb3daa4bff7f662eb2
Zeb McCorkle <zmccorle@creativevg.tk>
2013-07-23 18:38:20 -0400

    Creates black screen instead of "It's alive!"

--- src/main/java/tk/creativevg/Newverse/MainMenu.java
+++ src/main/java/tk/creativevg/Newverse/MainMenu.java
@@ -1,10 +1,30 @@
 package tk.creativevg.Newverse;
 
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
 public class MainMenu {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("It's alive!");
+		try {
+			Display.setDisplayMode(new DisplayMode(640, 480));
+			Display.setTitle("Newverse");
+			Display.create();
+		} catch (LWJGLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Display.destroy();
+			System.exit(1);
+		}
+		
+		while ( !Display.isCloseRequested() ) {
+			
+			Display.update();
+			Display.sync(60);
+			
+		} Display.destroy(); System.exit(0);
 		
 	}
 	
